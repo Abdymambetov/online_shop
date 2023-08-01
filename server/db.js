863553e91ecf7c1cabaa47ba255dadbf2ebfb5b5
Abdymambetov <meder0555@icloud.com>
2023-08-01 22:44:57 +0600

    initial commit

---
+++ server/db.js
@@ -0,0 +1,13 @@
+
+const {Sequlize} = require('sequelize')
+
+module.exports = new Sequlize(
+    process.env.DB_NAME, //название базы данных
+    process.env.DB_USER, // пользователь
+    process.env.DB_PASSWORD, // пароль
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+)
